066fc129da4201c7c2981b18de7ec8d575228903
Luka Miličević <luka.milicevic147@gmail.com>
2024-10-14 18:36:01 +0200

    views

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import path from 'path';
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist/views')));
+app.use(express.static(path.join(__dirname, 'dist/public')));
+app.set('views', path.join(__dirname, 'dist/views'));
 app.set("view engine", "ejs");
 
 const externalUrl = process.env.EXTERNAL_URL || null;
